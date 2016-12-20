a48e35902140a4d5c2f31162b8282914efeff5c4
Oleksandr Badyornyi <badyornyi.o@gmail.com>
2016-12-20 23:48:48 +0200

    All tasks for Data Classes HW are finished (iteration 1)

--- main_data_classes.rb
+++ main_data_classes.rb
@@ -2,7 +2,7 @@
 
 #it has strange behaviour in case when 1st digit of number is 0 (such as 012, 0177, etc)
 #code recognize them as octal numbers
-def sum_of_digits (nat_num)
+def sum_of_digits(nat_num)
   if nat_num.class.is_a?(Fixnum) && nat_num >= 0
     str_digits = nat_num.to_s
     arr_digits = str_digits.split('')
@@ -25,7 +25,7 @@ end
 #2. There is a string. Find a maximum number of digits placed in a row in it
 
 #str = 'qw12e63406q4qeg156g16w1'
-def digits_maxnum_in_string (str)
+def digits_maxnum_in_string(str)
   arr = Array.new
   max_length = 0
   str.chars do |char|
@@ -52,7 +52,7 @@ end
 #3. There is a string. Find a maximal number (not a digit) in it
 
 #str = 'qw10e614q4qeg156g16w1'
-def max_number_in_string (str)
+def max_number_in_string(str)
   arr = Array.new
   number = 0
   temp_num = 0
@@ -91,15 +91,36 @@ end
 #4. There are two strings. Find number of first chars from first string matching first chars of the second string. Consider two cases:
 #- strings are definitely different
 #- strings can completely match
-#str1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
-#str2 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
-#str3 = 'auqila angam erolod te erobal tu tnudidicni ropmet domsuie od des ,tile gnicsipida rutetcesnoc ,tema tis rolod muspi meroL'
+#str1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+#str2 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+#str2 = 'Lorem ipsum sit amet, consectetur adipiscing elit'
+#str2 = 'Lorem456'
+#str2 = 'orem ipsum'
+#str2 = ''
+
+def strings_matching(str1, str2)
+  arr1 = str1.split('')
+  arr2 = str2.split('')
+
+  if arr1.length <= arr2.length
+    length = arr1.length
+  else
+    length = arr2.length
+  end
+
+  number = 0
+  while number<length && arr1[number] == arr2[number]
+    number+=1
+  end
+  puts "Number is #{number}"
+  #return number
+end
 
 
 #5. There is an array of integers. First puts elements with even indexes and then with odd indexes
 
 #ary = [1,2,3,4,5,6,7,8,9]
-def even_and_odd_indexes (ary)
+def even_and_odd_indexes(ary)
   puts 'Elements with even indexes are:'
   i = 0
   while i < ary.length
@@ -118,7 +139,7 @@ end
 #6. There is an array of integers (ary). Puts the index of the last element where ary[0]<ary[i]<ary[-1]
 
 #ary = [1,2,3,4,5,6,7,8,9]
-def arr_last_element_index (ary)
+def arr_last_element_index(ary)
   last_index = ary.length - 1
   puts "Last index of your array is #{last_index}"
 end
@@ -127,7 +148,7 @@ end
 #7. There is an array of integers (ary). Modify it with adding ary[0] (first element of the array) to each even number. Don't do it for first and last elements
 
 #ary = [2,3,8,2,5,6,7,2,8]
-def modify_even_number_in_ary (ary)
+def modify_even_number_in_ary(ary)
   i = 1
   last_index = ary.length - 1
   while i < last_index
@@ -143,7 +164,7 @@ end
 #8. There is a hash where keys and values are strings. Modify it: all keys should be symbols and all values should be integers. If you couldn't modify some value set it to nil
 
 #h = {'Lorem' => '1', 'dolor' => ':sit', 'amet' => '-3', 'adipiscing' => 'elit','sed' => 'do', 'eiusmod' => '0'}
-def hash_str_to_sym_and_int (h)
+def hash_str_to_sym_and_int(h)
   a_keys = h.keys
   a_values = h.values
 
@@ -171,7 +192,7 @@ end
 #9. There is a hash where keys are symbols and values are integers. Modify it: remove all pairs where symbols start with "s" character
 
 #h = {Lorem: 1, solor: 3, smet: 5, adipiscing: 7, Sed: 9, eiusmod: 0}
-def hash_rem_pair_key_start_s_char (h)
+def hash_rem_pair_key_start_s_char(h)
   h.delete_if do |key, value|
     key.to_s.split('')[0] == 's'
   end
@@ -182,7 +203,7 @@ end
 #10. There is a hash where keys are symbols and values are integers. Modify it: leave only pairs where value is natural number and more than 0
 
 #h = {Lorem: 1, solor: 3, smet: -5, adipiscing: -7, Sed: 9, eiusmod: 0}
-def hash_leave_pair_val_natural (h)
+def hash_leave_pair_val_natural(h)
   h.delete_if do |key, value|
     value <= 0
   end
