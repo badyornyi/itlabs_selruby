1985c829b2d2e868778c2dad9aa7737b95ca6e34
Oleksandr Badyornyi <badyornyi.o@gmail.com>
2017-02-13 23:21:33 +0200

    simple model creation started

--- object_class_method_variable/scenarios_trello.rb
+++ object_class_method_variable/scenarios_trello.rb
@@ -6,6 +6,51 @@
 #-cards
 #-comments
 #Create 10 any unit tests for this classes and their methods
-#2. Add a User model to your project and modify all methods which work with user data (email or password) to treat users in a right way
 
+#uses
+#create user
+user_register
+user_login
+user_logout
 
+#boards
+#create 4 different boards
+board_create
+#mark 1st board as favorite
+board_favorite
+#rename 2nd board
+board_rename
+#delete 4th board
+board_delete
+
+#lists
+#create 4 lists on 1st board
+list_create
+#copy 1st list
+list_copy
+#rename 2nd list
+list_rename
+#move 3rd list to 3rd board
+list_move
+#archive 4th list
+list_archive
+
+#cards
+#add 4 cards to 1st list
+card_create
+#rename 1st card
+card_rename
+#copy 2nd card to 2nd list
+card_copy
+#move 3rd card to trd list on 3rd board
+card_move
+#archive 4th card
+card_archive
+
+#comments
+#add comment to 1st card
+comment_add
+#edit comment to 1st card
+comment_edit
+#delete comment to 1st card
+comment_delete
